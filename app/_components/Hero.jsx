407bf301ec9025a8684cd407a865fd2459fba396
kritika72005 <kritikasinghbaghel3@gmail.com>
2024-09-10 13:00:03 +0530

    Added landing page

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <section className="bg-gray-50 flex items-center flex-col">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold  text-black sm:text-5xl">
+            Manage Your Expanses
+            <strong className="font-extrabold text-primary sm:block">
+              {" "}
+              Control Your Money{" "}
+            </strong>
+          </h1>
+
+          <p className="mt-4  text-black sm:text-xl/relaxed">
+            Start Creating your budget and save ton of money
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              href="#"
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>
+      <Image
+        src={"/dashboard.png"}
+        alt="dashboard"
+        width={1000}
+        height={700}
+        className="-mt-9 round-xl border-2"
+      />
+    </section>
+  );
+}
+
+export default Hero;
---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
